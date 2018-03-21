6b4dc3b49801cf90ffa026e09f01cf6ebc6ccd64
Vladymyr <poptsov_vladymyr@yahoo.com>
2018-03-21 16:56:49 +0200

    Добавлен тригер, переделанно логирование

--- ZohoHelperClasses/ZohoMethods.php
+++ ZohoHelperClasses/ZohoMethods.php
@@ -11,27 +11,22 @@ class ZohoMethods
      *
      * @param string $modulename API имя модуля
      * @param string $jsondata JSON заполненый {поле : изначение,...} создаваемой сущности
+     * @param string $wfTriger тригер ставится только чтобы срабатывала связная задача, виды: approval,workflow,blueprint
      */
-    public static function CreateRecord($modulename, $jsondata)
+    public static function CreateRecord($modulename, $jsondata, $wfTriger)
     {
         $url = "https://www.zohoapis.com/crm/v2/" . $modulename;
-        $data = '{
-	                "data": ['
-            . $jsondata .
-            ']
-                 }';
-        $result = ZohoConnector::Requesting($url, $data,'POST');
-        if (isset($result)){
-            $jsonerror = json_decode($result,true);
-            if ($jsonerror['status'] === "error"){
-                $fp = fopen(dirname(__FILE__) .'/ErrorsLog.txt',"w");
-                fwrite($fp,"Error: ".$result."\n");
-                fclose($fp);
+        $data = '{"data": ['. $jsondata .'],"trigger": ['.$wfTriger.']}';
+        $result = ZohoConnector::Requesting($url, $data, 'POST');
+        if (isset($result)) {
+            $jsonerror = json_decode($result, true);
+            if ($jsonerror['status'] === "error") {
+                file_put_contents(dirname(__FILE__) .'/ErrorsLog.txt', date('Y-m-d H:i:s') . "Error $modulename: ".$result."\n", FILE_APPEND);
             } else {
                 return $result;
             }
         }
-            return null;
+        return null;
     }
 
     /**
@@ -41,22 +36,17 @@ class ZohoMethods
      * @param string $modulename API имя модуля
      * @param string $id идентификатор сущности
      * @param string $jsondata JSON заполненый {поле : изначение,...} обновляемой сущности
+     * @param string $wfTriger тригер ставится только чтобы срабатывала связная задача, виды: approval,workflow,blueprint
      */
-    public static function UpdateRecord($modulename, $id, $jsondata)
+    public static function UpdateRecord($modulename, $id, $jsondata,$wfTriger)
     {
         $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $id;
-        $data = '{
-	                "data": ['
-            . $jsondata .
-            ']
-                 }';
-        $result = ZohoConnector::Requesting($url, $data,'PUT');
-        if (isset($result)){
-            $jsonerror = json_decode($result,true);
-            if ($jsonerror['status'] === "error"){
-                $fp = fopen(dirname(__FILE__) .'/ErrorsLog.txt',"w");
-                fwrite($fp,"Error: ".$result."\n");
-                fclose($fp);
+        $data = '{"data": ['. $jsondata .'],"trigger": ['.$wfTriger.']}';
+        $result = ZohoConnector::Requesting($url, $data, 'PUT');
+        if (isset($result)) {
+            $jsonerror = json_decode($result, true);
+            if ($jsonerror['status'] === "error") {
+                file_put_contents(dirname(__FILE__) .'/ErrorsLog.txt', date('Y-m-d H:i:s') . "Error $modulename: ".$result."\n", FILE_APPEND);
             } else {
                 return $result;
             }
@@ -73,13 +63,11 @@ class ZohoMethods
     public static function DeleteRecord($modulename, $id)
     {
         $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $id;
-        $result = ZohoConnector::Requesting($url,'','DELETE');
-        if (isset($result)){
-            $jsonerror = json_decode($result,true);
-            if ($jsonerror['status'] === "error"){
-                $fp = fopen(dirname(__FILE__) .'/ErrorsLog.txt',"w");
-                fwrite($fp,"Error: ".$result."\n");
-                fclose($fp);
+        $result = ZohoConnector::Requesting($url, '', 'DELETE');
+        if (isset($result)) {
+            $jsonerror = json_decode($result, true);
+            if ($jsonerror['status'] === "error") {
+                file_put_contents(dirname(__FILE__) .'/ErrorsLog.txt', date('Y-m-d H:i:s') . "Error $modulename: ".$result."\n", FILE_APPEND);
             } else {
                 return $result;
             }
@@ -96,22 +84,20 @@ class ZohoMethods
     public static function GetRecord($modulename, $id)
     {
         $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $id;
-        $result = ZohoConnector::Requesting($url,'','GET');
-        if (isset($result)){
-            $jsonerror = json_decode($result,true);
-            if ($jsonerror['status'] === "error"){
-                $fp = fopen(dirname(__FILE__) .'/ErrorsLog.txt',"w");
-                fwrite($fp,"Error: ".$result."\n");
-                fclose($fp);
+        $result = ZohoConnector::Requesting($url, '', 'GET');
+        if (isset($result)) {
+            $jsonerror = json_decode($result, true);
+            if ($jsonerror['status'] === "error") {
+                file_put_contents(dirname(__FILE__) .'/ErrorsLog.txt', date('Y-m-d H:i:s') . "Error $modulename: ".$result."\n", FILE_APPEND);
             } else {
                 $jsonstr = json_decode($result);
                 $jsonelem = $jsonstr->data;
                 $jsonenc = json_encode($jsonelem);
-                $jsonobj = substr($jsonenc,1,strlen($jsonenc)-2);
+                $jsonobj = substr($jsonenc, 1, strlen($jsonenc) - 2);
                 return $jsonobj;
             }
         }
-            return null;
+        return null;
     }
 
     /**
@@ -123,18 +109,16 @@ class ZohoMethods
     public static function SearchRecordByCriteria($modulename, $criteria)
     {
         $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/search?criteria=" . $criteria;
-        $result = ZohoConnector::Requesting($url,'','GET');
-        if (isset($result)){
-            $jsonerror = json_decode($result,true);
-            if ($jsonerror['status'] === "error"){
-                $fp = fopen(dirname(__FILE__) .'/ErrorsLog.txt',"w");
-                fwrite($fp,"Error: ".$result."\n");
-                fclose($fp);
+        $result = ZohoConnector::Requesting($url, '', 'GET');
+        if (isset($result)) {
+            $jsonerror = json_decode($result, true);
+            if ($jsonerror['status'] === "error") {
+                file_put_contents(dirname(__FILE__) .'/ErrorsLog.txt', date('Y-m-d H:i:s') . "Error $modulename: ".$result."\n", FILE_APPEND);
             } else {
                 $jsonstr = json_decode($result);
                 $jsonelem = $jsonstr->data;
                 $objlist = array();
-                foreach ($jsonelem as $key){
+                foreach ($jsonelem as $key) {
                     $objlist[] = json_encode($key);
                 }
                 return $objlist;
@@ -152,24 +136,23 @@ class ZohoMethods
      */
     public static function GetRelatedRecords($modulename, $relaterecordname, $id)
     {
-        $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $id."/".$relaterecordname;
-        $result = ZohoConnector::Requesting($url,'','GET');
-        if (isset($result)){
-            $jsonerror = json_decode($result,true);
-            if ($jsonerror['status'] === "error"){
-                $fp = fopen(dirname(__FILE__) .'/ErrorsLog.txt',"w");
-                fwrite($fp,"Error: ".$result."\n");
-                fclose($fp);
+        $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $id . "/" . $relaterecordname;
+        $result = ZohoConnector::Requesting($url, '', 'GET');
+        if (isset($result)) {
+            $jsonerror = json_decode($result, true);
+            if ($jsonerror['status'] === "error") {
+                file_put_contents(dirname(__FILE__) .'/ErrorsLog.txt', date('Y-m-d H:i:s') . "Error $modulename: ".$result."\n", FILE_APPEND);
             } else {
                 $jsonstr = json_decode($result);
                 $jsonelem = $jsonstr->data;
                 $jsonenc = json_encode($jsonelem);
-                $jsonobj = substr($jsonenc,1,strlen($jsonenc)-2);
+                $jsonobj = substr($jsonenc, 1, strlen($jsonenc) - 2);
                 return $jsonobj;
             }
         }
         return null;
     }
+
     /**
      * обновляем связанный модуль у сущности из ZohoCRM
      *
@@ -178,22 +161,17 @@ class ZohoMethods
      * @param string $relaterecordname API имя связанного модуля
      * @param string $relatedrecordid идентификатор связного списка
      * @param string $jsondata JSON заполненый {поле : изначение,...} обновляемого связного модуля
+     * @param string $wfTriger тригер ставится только чтобы срабатывала связная задача, виды: approval,workflow,blueprint
      */
-    public static function UpdateRelatedRecord($modulename, $moduleid, $relaterecordname, $relatedrecordid, $jsondata)
+    public static function UpdateRelatedRecord($modulename, $moduleid, $relaterecordname, $relatedrecordid, $jsondata, $wfTriger)
     {
-        $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $moduleid."/".$relaterecordname."/".$relatedrecordid;
-        $data = '{
-	                "data": ['
-            . $jsondata .
-            ']
-                 }';
-        $result = ZohoConnector::Requesting($url, $data,'PUT');
-        if (isset($result)){
-            $jsonerror = json_decode($result,true);
-            if ($jsonerror['status'] === "error"){
-                $fp = fopen(dirname(__FILE__) .'/ErrorsLog.txt',"w");
-                fwrite($fp,"Error: ".$result."\n");
-                fclose($fp);
+        $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $moduleid . "/" . $relaterecordname . "/" . $relatedrecordid;
+        $data = '{"data": ['. $jsondata .'],"trigger": ['.$wfTriger.']}';
+        $result = ZohoConnector::Requesting($url, $data, 'PUT');
+        if (isset($result)) {
+            $jsonerror = json_decode($result, true);
+            if ($jsonerror['status'] === "error") {
+                file_put_contents(dirname(__FILE__) .'/ErrorsLog.txt', date('Y-m-d H:i:s') . "Error $modulename: ".$result."\n", FILE_APPEND);
             } else {
                 return $result;
             }
@@ -206,10 +184,12 @@ class ZohoMethods
      *
      * @param string $jsonobj JSON объект сущности
      * @param string $valueApiname API имя поля
+     *
      */
-    public static function GetFieldValue ($jsonobj,$valueApiname){
-        $jsonobjDec = json_decode($jsonobj,true);
-        if (is_array($jsonobjDec[$valueApiname])){
+    public static function GetFieldValue($jsonobj, $valueApiname)
+    {
+        $jsonobjDec = json_decode($jsonobj, true);
+        if (is_array($jsonobjDec[$valueApiname])) {
             $inneronj = $jsonobjDec[$valueApiname];
             return $inneronj['id'];
         } else {
@@ -217,21 +197,31 @@ class ZohoMethods
         }
     }
 
-    public static function UpdateFieldValue ($modulename, $id ,$valueApiname, $value){
+    /**
+     * Получает значение поля, если поле это лукап получаем идентификатор вложенного поля
+     *
+     * @param string $modulename API имя модуля
+     * @param string $id идентификатор модуля
+     * @param string $valueApiname API имя поля
+     * @param string $value значение поля
+     * @param string $wfTriger тригер ставится только чтобы срабатывала связная задача, виды: approval,workflow,blueprint
+     */
+    public static function UpdateFieldValue($modulename, $id, $valueApiname, $value, $wfTriger)
+    {
         $record = ZohoMethods::GetRecord($modulename, $id);
-        if(isset($record)){
-            $jsonobjDec = json_decode($record,true);
+        if (isset($record)) {
+            $jsonobjDec = json_decode($record, true);
             $url = "https://www.zohoapis.com/crm/v2/" . $modulename . "/" . $id;
-            if (is_array($jsonobjDec[$valueApiname])){
-                $data = '{"data": [{'. $valueApiname .':{"id":'.$value.'}}]}';
-                $result = ZohoConnector::Requesting($url, $data,'PUT');
-                if (isset($result)){
+            if (is_array($jsonobjDec[$valueApiname])) {
+                $data = '{"data": [{' . $valueApiname . ':{"id":' . $value . '}}],"trigger": ['.$wfTriger.']}';
+                $result = ZohoConnector::Requesting($url, $data, 'PUT');
+                if (isset($result)) {
                     return $result;
                 }
             } else {
-                $data = '{"data":[{'. $valueApiname .':'.$value.'}]}';
-                $result = ZohoConnector::Requesting($url, $data,'PUT');
-                if (isset($result)){
+                $data = '{"data":[{' . $valueApiname . ':' . $value . '}],"trigger": ['.$wfTriger.']}';
+                $result = ZohoConnector::Requesting($url, $data, 'PUT');
+                if (isset($result)) {
                     return $result;
                 }
             }
